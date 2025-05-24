ff29f2019f647f01e4b55e432b7db3ebf4d2f04b
Rinzin Dolma <rsamrab7@gmail.com>
2025-05-24 13:09:33 -0400

    Added part 2

--- script.js
+++ script.js
@@ -15,12 +15,40 @@ const adventurer = {
   },
   roll (mod=0){
     const result= Math.floor(Math.random()* 20)+mod+1;
-    console.log(`${this.name} rolled a ${result}`)
+    // console.log(`${this.name} rolled a ${result}`)
   }
 };
 for (let item of adventurer.inventory) {
   // console.log(item);
 }
 adventurer.roll()
+
+//Part 2 Class Fantasy
+
+class Character {
+  constructor(name){
+this.name= name;
+this.health=100;
+this.inventory=[];
+  }
+  static Max_Health =100;
+
+  roll(mod=0){
+const result= Math.floor(Math.random()*20 + mod+1);
+console.log(`${this.name} rolled a ${result}`)
+  }
+}
+// const Robin= new Character("Robin");
+// robin.inventory=["sword", "potion", "artifact"];
+// robin.companion=new Character("Leo");
+// robin.companion.type="Cat"
+// robin.companion.companion=new Character("Frank");
+// robin.companion.companion.type="Flea";
+// robin.companion.companion.inventory=["small hat", "sunglasses"];
+
+// robin.roll();
+// robin.companion.roll();
+
+
 
 
